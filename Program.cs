6c4006ec2b2103ee216601519237409db24b89e3
Fabio-F16 <fipsfabio95@gmail.com>
2022-07-01 11:38:09 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace csharp_banca_oop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
